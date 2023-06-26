adef6b8e14bee6a60f92b1f120629e9be3171a3e
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-06-26 14:37:43 +1000

    initial commit

---
+++ Rock Paper Scissors/script.js
@@ -0,0 +1,44 @@
+//**********************************************************************************
+// written by Ben Jones
+//
+// Student boilerplate to make a OOP Rock Paper Scissors
+// - OOP - Const data type - Passing data into methods
+//
+// Written 22/6/2023
+// License: https://github.com/TempeHS/2024SDD-WebJS-Ben.Jones/blob/main/LICENSE
+//**********************************************************************************
+
+
+
+let playerScore;
+let computerScore;
+
+function gameLoop (playerClicked) {
+	const randomNumber = generateRandomNumber()
+	// convert random number to a choice so you can both equality check and use in the ouput string it should be stored in a const 
+	
+	
+	
+	const computerChoice = randomNumber //for debug only
+	equalityCheck (playerClicked, computerChoice);
+}
+
+function generateRandomNumber () {
+	const result = Math.floor((Math.random() * 3) + 1);
+	return result;
+}
+
+
+function equalityCheck (playerChoice, computerChoice){
+	// perform an equality check, update scores and output a string using concatenation that gives user feedback 
+
+
+
+	alert (playerChoice);  //for debug only
+	alert (computerChoice); //for debug only
+
+	const playerTip = 0;
+	document.getElementById("playerScoreContent").innerHTML = playerScore;
+	document.getElementById("computerScoreContent").innerHTML = computerScore;
+	document.getElementById("tipContent").innerHTML = playerTip;
+}
