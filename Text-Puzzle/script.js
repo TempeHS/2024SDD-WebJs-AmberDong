8789a79002dc22e41a1c7dc8fc488bf022bc9bd9
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-06-28 09:06:15 +1000

    initial commit

---
+++ Text-Puzzle/script.js
@@ -0,0 +1,11 @@
+//**********************************************************************************
+// written by Ben Jones
+//
+// Student idea container, students use tehir HTML, CSS & JS skills to turn this into a word puzzle game.
+//
+// Written 23/6/2023
+// License: https://github.com/TempeHS/2024SDD-WebJS-Ben.Jones/blob/main/LICENSE
+//**********************************************************************************
+
+var x = document.getElementById("playerWin");
+x.style.display = "block";
