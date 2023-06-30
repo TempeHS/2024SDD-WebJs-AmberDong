8a943bec010349253a3fcdad73ae834972753b36
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-06-30 20:53:00 +1000

    initial commit

--- Text-Puzzle/script.js
+++ Text-Puzzle/script.js
@@ -7,10 +7,20 @@
 // License: https://github.com/TempeHS/2024SDD-WebJS-Ben.Jones/blob/main/LICENSE
 //**********************************************************************************
 
-var x = document.getElementById("playerWin");
-x.style.display = "block";
-x.style.fontStyle = "italic";
-x.style.color = "black";
+let x = document.getElementById("playerWin");
+let once = document.getElementById("1");
+let two = document.getElementById("2");
+let three = document.getElementById("3");
+
+
+
+function gameLoop(playerClicked) {
+    if (once == 1) {
+        once.style.display = "block";
+    }
+    x.style.display = "block";
+    x.style.fontStyle = "italic";
+}
 
 
 
