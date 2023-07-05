fb1d897f51b9de5b0797d82d4c2b56dc4a1a1059
Amber <136765021+Amberdong27@users.noreply.github.com>
2023-07-06 01:08:46 +1000

    initial commit

--- Text-Puzzle/script.js
+++ Text-Puzzle/script.js
@@ -8,22 +8,30 @@
 //**********************************************************************************
 
 let x = document.getElementById("playerWin");
-let once = document.getElementById("1");
+let one = document.getElementById("1");
 let two = document.getElementById("2");
 let three = document.getElementById("3");
+let numberCounter = 0;
 
 
 
 function gameLoop(playerClicked) {
-    if (once == 1) {
-        once.style.display = "block";
+    if (numberCounter == 0 && playerClicked == 'once') {
+        numberCounter = numberCounter + 1;
+        one.style.fontWeight = 700;
+        one.style.color = "red";
+    } else {
+        if (numberCounter == 1 && playerClicked == 'two') {
+            numberCounter = numberCounter + 1;
+            two.style.fontWeight = 700;
+            two.style.color = "red";
+        } else {
+            if (numberCounter == 2 && playerClicked == 'three') {
+                three.style.fontWeight = 700;
+                three.style.color = "red";
+                x.style.display = "block";
+                x.style.fontStyle = "italic";
+            }
+        }
     }
-    x.style.display = "block";
-    x.style.fontStyle = "italic";
 }
-
-
-
-
-
-
